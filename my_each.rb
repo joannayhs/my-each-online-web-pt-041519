07e049fddad760739cd3ae6bf7e3e2d664964003
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 19:42:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,6 +4,6 @@ def my_each(array) # put argument(s) here
   while counter >= array.length 
   counter += 1 
  end 
- my_each(array){|item| puts "#{item}"
+ my_each(array){|item| puts "#{item}"}
 end
 
